5b2a97bdf2432161103fae61a7b8becbf3bcaa3d
schidler <t.online@qq.com>
2022-03-04 17:18:28 +0800

    for fix

--- start.sh
+++ start.sh
@@ -1,12 +1,32 @@
 ROOT_PATH=`pwd`
 
+N=`ps aux | grep "genieacs-nbi" | grep -v grep | awk '{print $2}' | wc -l`
+
+[ $N -eq 0 ] && {
 ${ROOT_PATH}/dist/bin/genieacs-nbi &
 sleep 2
+}
+
+N=`ps aux | grep "genieacs-fs" | grep -v grep | awk '{print $2}' | wc -l`
+[ $N -eq 0 ] && {
 ${ROOT_PATH}/dist/bin/genieacs-fs &
 sleep 2
+}
+
+N=`ps aux | grep "genieacs-ext" | grep -v grep | awk '{print $2}' | wc -l`
+[ $N -eq 0 ] && {
 ${ROOT_PATH}/dist/bin/genieacs-ext &
 sleep 2
+}
+
+N=`ps aux | grep "genieacs-cwmp" | grep -v grep | awk '{print $2}' | wc -l`
+[ $N -eq 0 ] && {
 ${ROOT_PATH}/dist/bin/genieacs-cwmp &
 sleep 2
+}
+
+N=`ps aux | grep "genieacs-ui" | grep -v grep | awk '{print $2}' | wc -l`
+[ $N -eq 0 ] && {
 ${ROOT_PATH}/dist/bin/genieacs-ui --ui-jwt-secret secret &
 sleep 2
+}
